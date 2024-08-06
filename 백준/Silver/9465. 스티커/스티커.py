4e80e2557b60939803b11fa4b46713d94d7f3431
song9984 <54995955+SongSeungYun@users.noreply.github.com>
2024-08-07 01:56:24 +0900

    [Silver I] Title: 스티커, Time: 236 ms, Memory: 168184 KB -BaekjoonHub

--- 백준/Silver/9465. 스티커/스티커.py
+++ 백준/Silver/9465. 스티커/스티커.py
@@ -12,6 +12,6 @@ for _ in range(t):
     dp[0][1]=l[1][0]+l[0][1]
     dp[1][1]=l[0][0]+l[1][1]
     for i in range(2,n):
-        dp[0][i]=l[0][i]+max(dp[1][i-1],dp[0][i-2],dp[1][i-2])
-        dp[1][i]=l[1][i]+max(dp[0][i-1],dp[0][i-2],dp[1][i-2])
-    print(max(dp[0][n-2],dp[1][n-2],dp[0][n-1],dp[1][n-1]))
+        dp[0][i]=l[0][i]+max(dp[1][i-1],dp[1][i-2])
+        dp[1][i]=l[1][i]+max(dp[0][i-1],dp[0][i-2])
+    print(max(dp[0][n-1],dp[1][n-1]))
